53753b1cd7400a4598e660965a503ee22f87a956
Adam Bohannon <abo46n2@gmail.com>
2017-11-04 20:07:36 -0700

    Attaching data to DOM

--- script.js
+++ script.js
@@ -8,10 +8,31 @@ const app = (data) => {
     summary,
     humidity
   } = data.currently
+
+  const tempEl = document.querySelector('.container__temp')
+  tempEl.innerHTML = `<span>${temperature}</span>`
+
+  const imageEl = document.querySelector('.container__image')
+  imageEl.innerHTML = `<span>${summary}</span>`
 }
+
+// fetch user's IP address to get latitude and longitude
+const IP_URL = 'https://ipapi.co/json/'
+
+fetch(IP_URL)
+  .then((response) => {
+    if (response.status !== 200) {
+      console.log('Problem with ip response', response.status)
+    } else {
+      return response.json()
+    }
+  })
+  .then((data) => getWeather(data))
+  .catch((error) => console.log('There was an error. IP promise rejected.', error))
 
 // fetch local weather data using user data
 const getWeather = (userData) => {
+  console.log('user', userData)
   const {latitude, longitude} = userData
   const API_KEY = 'f563956b1d0ba587267bc6744f0aeeb6'
   const weatherAPI = `https://api.darksky.net/forecast/${API_KEY}/${latitude},${longitude}`
@@ -29,11 +50,3 @@ const getWeather = (userData) => {
     .then((data) => app(data))
     .catch((error) => console.log('There was an error. Weather promise rejected.', error))
 }
-
-// fetch user's IP address to get latitude and longitude
-const IP_URL = 'https://ipapi.co/json/'
-
-fetch(IP_URL)
-  .then((response) => response.json())
-  .then((data) => getWeather(data))
-  .catch((error) => console.log('There was an error. IP promise rejected.', error))
