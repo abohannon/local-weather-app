83ca0a8af078cedcad4a987161784cd572832093
Adam Bohannon <abo46n2@gmail.com>
2017-12-03 00:10:27 -0800

    Added click handler, state, and temp conversion function

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ const IP_URL = 'https://ipapi.co/json/'
 
 class WeatherApp {
   constructor () {
-    this.tempEl = document.querySelector('.app__primary__top__temp')
+    this.currentTemp = document.querySelector('.current-temp')
+    this.currentHighLow = document.querySelector('.highlow-temp')
     this.imageEl = document.querySelector('.app__primary__icon')
     this.cityEl = document.querySelector('.app__primary__top__city')
+    this.summary = document.querySelector('.app__primary__summary')
     this.secondaryTempOne = document.querySelector('.app__secondary__temp--one')
     this.secondaryTempTwo = document.querySelector('.app__secondary__temp--two')
     this.secondaryTempThree = document.querySelector('.app__secondary__temp--three')
@@ -17,7 +19,25 @@ class WeatherApp {
     this.secondaryIconThree = document.querySelector('.app__secondary__icon--three')
     this.getUserData = this.getUserData.bind(this)
     this.getWeather = this.getWeather.bind(this)
+    console.log(this.currentTemp)
+
+    this.state = {
+      celsius: false
+    }
   }
+
+  convertToCelsius (fahrenheit) {
+    const celsius = (fahrenheit - 32) / 1.8
+    return celsius.toFixed(1)
+  }
+
+  handleClick () {
+    this.currentTemp.addEventListener('click', () => {
+      this.state.celsius = !this.state.celsius
+      this.getUserData()
+    })
+  }
+
   // fetch the user IP data
   getUserData () {
     fetch(IP_URL)
@@ -47,7 +67,6 @@ class WeatherApp {
           return response.json()
         }
       }).then((weatherData) => {
-        console.log('inside second fetch', weatherData)
         this.renderWeatherData({weatherData, userData})
       }).catch((error) => console.log(error))
   }
@@ -58,17 +77,42 @@ class WeatherApp {
     const {temperature, summary} = data.weatherData.currently
     const {currently, daily} = data.weatherData
     const {city} = data.userData
+    let tempToday
+    let highToday
+    let lowToday
+    let highOneDay
+    let lowOneDay
 
-    this.tempEl.innerHTML = `<span>${temperature}</span>`
+    if (!this.state.celsius) {
+      tempToday = `${temperature.toFixed(1)}&deg;F`
+      highToday = daily.data[0].temperatureHigh.toFixed(1)
+      lowToday = daily.data[0].temperatureLow.toFixed(1)
+
+      highOneDay = daily.data[1].temperatureHigh.toFixed(1)
+      lowOneDay = daily.data[1].temperatureLow.toFixed(1)
+    } else if (this.state.celsius) {
+      tempToday = this.convertToCelsius(temperature.toFixed(1)) + '&deg;C'
+      highToday = this.convertToCelsius(daily.data[0].temperatureHigh.toFixed(1))
+      lowToday = this.convertToCelsius(daily.data[0].temperatureLow.toFixed(1))
+
+      highOneDay = this.convertToCelsius(daily.data[1].temperatureHigh.toFixed(1))
+      lowOneDay = this.convertToCelsius(daily.data[1].temperatureLow.toFixed(1))
+    }
+
     this.cityEl.innerHTML = `<span>${city}</span>`
+
     // temp
-    this.secondaryTempOne.innerHTML = `${daily.data[0].temperatureHigh}`
-    this.secondaryTempTwo.innerHTML = `${daily.data[1].temperatureHigh}`
-    this.secondaryTempThree.innerHTML = `${daily.data[2].temperatureHigh}`
+    this.currentTemp.innerHTML = tempToday
+    this.currentHighLow.innerHTML = `${highToday} <span class="low-temp">/ ${lowToday}</span>`
+    this.secondaryTempOne.innerHTML = `${highOneDay} <span class="low-temp">/ ${lowOneDay}</span>`
+    this.secondaryTempTwo.innerHTML = `${daily.data[2].temperatureHigh.toFixed(1)} <span class="low-temp">/ ${daily.data[2].temperatureLow.toFixed(1)}</span>`
+    this.secondaryTempThree.innerHTML = `${daily.data[3].temperatureHigh.toFixed(1)} <span class="low-temp">/ ${daily.data[3].temperatureLow.toFixed(1)}</span>`
+
     // summary
-    this.secondarySummaryOne.innerHTML = `${daily.data[0].summary}`
-    this.secondarySummaryTwo.innerHTML = `${daily.data[1].summary}`
-    this.secondarySummaryThree.innerHTML = `${daily.data[2].summary}`
+    this.summary.innerHTML = `${currently.summary}`
+    this.secondarySummaryOne.innerHTML = `${daily.data[1].summary}`
+    this.secondarySummaryTwo.innerHTML = `${daily.data[2].summary}`
+    this.secondarySummaryThree.innerHTML = `${daily.data[3].summary}`
 
     // icons
     const icons = new Skycons({'color': 'white'})
@@ -83,6 +127,7 @@ class WeatherApp {
   // initialize the app by kicking off the first fetch for user data
   initialize () {
     this.getUserData()
+    this.handleClick()
   }
 }
 
